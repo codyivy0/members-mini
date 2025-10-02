86f3bc6f3d8cdc5c0bda2567c5080c0d57501714
Cody Ivy <codyivy0@gmail.com>
2025-10-02 14:10:34 -0500

    successful HTTP request on about page to HelloController

--- Frontend/member-frontend/src/pages/About.jsx
+++ Frontend/member-frontend/src/pages/About.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 export default function About() {
+  const [message, setMessage] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:8080/hello")
+      .then((response) => response.text())
+      .then((data) =>  setMessage(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div>
       <h1>ABOUT</h1>
+      <p>{message}</p>
     </div>
   );
 }
